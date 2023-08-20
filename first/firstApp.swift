92cdeaf7e0fe0e98a0ffd4a916224e65bb86821e
Svetlana_Purozhkova <svetlan-90@mail.ru>
2023-08-20 16:48:46 +0300

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by Светлана on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
